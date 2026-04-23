e564da8fe84d4fb9b2b6f231f9448707197cf499
DanielMuritiba <danielsmuritiba@gmail.com>
2026-04-23 20:33:28 -0300

    first commit

---
+++ src/Database/Connection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Src\Database;
+
+class Connection
+{
+    public static function make(): \PDO
+    {
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4',
+            $_ENV['DB_HOST'],
+            $_ENV['DB_PORT'],
+            $_ENV['DB_NAME']
+        );
+
+        return new \PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        ]);
+    }
+}
